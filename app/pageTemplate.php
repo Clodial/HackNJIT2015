2fc2380bf09d709bcae014170b6da1690fbc361c
homer4hire247 <goldman.adam.j@gmail.com>
2015-11-07 17:31:52 -0500

    Header and footer

--- app/pageTemplate.php
+++ app/pageTemplate.php
@@ -1,5 +1,7 @@
 <?php
 
+include "headFoot.css";
+
 abstract class pageTemplate{
 
 	/**
@@ -13,10 +15,23 @@ abstract class pageTemplate{
 
 	}
 	public function makeHeader(){
+		echo "
+			<div>
+                                <input type='search' id='mySearch' placeholder='
+Searching....'>
+                                <button >Search</button>
+        		</div>
 
+        		<ul>
+                		<li><a href='#home'>Home</a></li>
+                		<li><a href='#explore'>Explore</a></li>
+                		<li><a href='#account'>Account</a></li>
+                		<li><a href='#signin'>Sign In</a></li>
+
+        		</ul>";
 	}
 	public function makeFooter(){
-
+		echo "<div id='footer'>A Project by Matt and Adam</div>";
 	}
 	
 }
