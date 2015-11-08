17a29992a134c7e36ee1e714b5ad1b9b060b3b57
homer4hire247 <goldman.adam.j@gmail.com>
2015-11-07 21:06:37 -0500

    last commit for night

--- app/landPage.php
+++ app/landPage.php
@@ -2,6 +2,12 @@
 
 class landPage{
 
+	public function get() {
+		echo "<h1>Welcome to our Hack NJIT Project!!!!!</h1>
+			<br>
+			<p>This project is supposed to be gamification of a project manager sytem</p>
+		";
+	}
 	
 	
 }
